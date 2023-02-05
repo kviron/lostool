0df20f375529983ffad8e2fc9c99bca3a2e24791
kviron <kviron.web.dev@gmail.com>
2023-02-05 15:47:33 +0300

    добавил архитектуру

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
 import React, {Suspense, useContext, useState} from 'react';
 import {Link, Route, Routes} from 'react-router-dom'
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-
 import './styles/_index.scss'
-import {useTheme} from "./Theme/useTheme";
-import {classNames} from "./helpers/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
+import {classNames} from "shared";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme()
@@ -17,8 +16,8 @@ const App = () => {
             <Link to={'/about'}>О нас</Link>
             <Suspense fallback={<div>Загрузка...</div>}>
                 <Routes>
-                    <Route path={'/'} element={<MainPageAsync/>}/>
-                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                    <Route path={'/'} element={<MainPage/>}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
                 </Routes>
             </Suspense>
         </div>
---
+++ src/shared/index.ts
@@ -0,0 +1,5 @@
+import {classNames} from "./lib/classNames";
+
+export {
+    classNames
+}
